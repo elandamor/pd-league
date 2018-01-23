9d5dcdd6ad177f1f9d3eee1c6fc062ce582c6f38
Thandolwethu Mpofu <mpofuthandolwethu@gmail.com>
2018-01-24 01:46:46 +0200

    Replaced key with abbr

--- backend/resolvers.js
+++ backend/resolvers.js
@@ -4,8 +4,8 @@ import { clubs, standings, users } from './data';
 
 const resolvers = {
   Query: {
-    clubs: () => _.sortBy(clubs, ['key']),
-    club: (obj, { key }) => find(clubs, { key }),
+    clubs: () => _.sortBy(clubs, ['abbr']),
+    club: (obj, { abbr }) => find(clubs, { abbr }),
     standings: () => _.sortBy(standings, ['position.current']),
     users: () => users,
     user: (obj, { id }) => find(users, { id }),

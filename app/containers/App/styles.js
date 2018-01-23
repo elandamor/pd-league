1b6a5ff28d5319105815660eac74bc6675564262
Thandolwethu Mpofu <mpofuthandolwethu@gmail.com>
2018-01-24 01:42:37 +0200

    Added styles

--- app/containers/App/styles.js
+++ app/containers/App/styles.js
@@ -1,7 +1,11 @@
 import styled from 'styled-components';
 
 const Container = styled.div`
-
+  .c-app-header {
+    position: sticky;
+    top: 0;
+    z-index: 5;
+  }
 `;
 
 export default Container;

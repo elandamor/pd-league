a4193cde0a0b1d1220118ba015161b63b6ca4962
Thandolwethu Mpofu <mpofuthandolwethu@gmail.com>
2018-01-26 00:09:43 +0200

    Made Table loadable

--- app/pages/Tables/Loadable.js
+++ app/pages/Tables/Loadable.js
@@ -5,8 +5,10 @@
  */
 
 import Loadable from 'react-loadable';
+// Styled-Components
+import Placeholder from './styles';
 
 export default Loadable({
   loader: () => import('./index'),
-  loading: () => null,
+  loading: Placeholder,
 });

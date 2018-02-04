126b4759fa66041ca49ea1d31521e7eece971eb1
Thandolwethu Mpofu <mpofuthandolwethu@gmail.com>
2018-02-04 02:32:12 +0200

    Added fixtures to data sources

--- backend/data/index.js
+++ backend/data/index.js
@@ -1,9 +1,11 @@
 import clubs from './clubs';
+import fixtures from './fixtures';
 import standings from './standings';
 import users from './users';
 
 export {
   clubs,
+  fixtures,
   standings,
   users,
 };

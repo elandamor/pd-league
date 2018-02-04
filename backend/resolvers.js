b2df80ff29d878b74ad25bbfc60e0dcb323ff77b
Thandolwethu Mpofu <mpofuthandolwethu@gmail.com>
2018-02-04 02:30:56 +0200

    Added fixtures query

--- backend/resolvers.js
+++ backend/resolvers.js
@@ -1,11 +1,12 @@
 import { _, find } from 'lodash';
 
-import { clubs, standings, users } from './data';
+import { clubs, fixtures, standings, users } from './data';
 
 const resolvers = {
   Query: {
     clubs: () => clubs,
     club: (obj, { abbr }) => find(clubs, { abbr }),
+    fixtures: () => fixtures,
     standings: () => _.sortBy(standings, ['position.current']),
     users: () => users,
     user: (obj, { id }) => find(users, { id }),

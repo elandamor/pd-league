9ffab2028832097a1acfdbe986998d4854a413de
Thandolwethu Mpofu <mpofuthandolwethu@gmail.com>
2018-02-04 21:02:42 +0200

    Added results Query resolver

--- backend/resolvers.js
+++ backend/resolvers.js
@@ -1,12 +1,13 @@
 import { _, find } from 'lodash';
 
-import { clubs, fixtures, standings, users } from './data';
+import { clubs, fixtures, results, standings, users } from './data';
 
 const resolvers = {
   Query: {
     clubs: () => clubs,
     club: (obj, { abbr }) => find(clubs, { abbr }),
     fixtures: () => fixtures,
+    results: () => results,
     standings: () => _.sortBy(standings, ['position.current']),
     users: () => users,
     user: (obj, { id }) => find(users, { id }),

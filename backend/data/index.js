4bc475ed2869e5057a36fe435d38a243c3ae4cbb
Thandolwethu Mpofu <mpofuthandolwethu@gmail.com>
2018-02-04 21:03:04 +0200

    Added results dataset

--- backend/data/index.js
+++ backend/data/index.js
@@ -1,11 +1,13 @@
 import clubs from './clubs';
 import fixtures from './fixtures';
+import results from './results';
 import standings from './standings';
 import users from './users';
 
 export {
   clubs,
   fixtures,
+  results,
   standings,
   users,
 };

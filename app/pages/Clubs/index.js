0d8eb7599f328934cbf67d8f8ab40988d413aaa6
Thandolwethu Mpofu <mpofuthandolwethu@gmail.com>
2018-02-04 02:30:02 +0200

    Refactored Club route

--- app/pages/Clubs/index.js
+++ app/pages/Clubs/index.js
@@ -5,25 +5,37 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import { Switch, Route } from 'react-router-dom';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 // Components
 // import Icon from '../../components/Icon';
 // import { ICONS } from '../../components/Icon/constants';
 // Containers
 import GetClubs from '../../containers/GetClubs/Loadable';
-import GetClubByAbbr from '../../containers/GetClubByAbbr/Loadable';
+// Pages
+import Club from '../Club/Loadable';
 // Styled-Components
-import Wrapper from './styles';
+import Wrapper, { Masthead } from './styles';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class Clubs extends React.Component {
   render() {
+    const { match } = this.props;
     return (
       <Wrapper>
         <Helmet>
           <title>Premier League Clubs - Fixtures, Results, Stats & Profiles</title>
           <meta name="description" content="Find profiles and information about current and all-time pdLeague clubs, on the official website of the pdLeague." />
         </Helmet>
+        {
+          match.isExact && (
+            <Masthead role="banner">
+              <div className="c-inner">
+                <h2 className="c-title">Clubs</h2>
+                <button className="c-bttn-share" />
+              </div>
+            </Masthead>
+          )
+        }
         <Switch>
           <Route
             exact
@@ -35,7 +47,7 @@ class Clubs extends React.Component {
           <Route
             path="/clubs/:abbr"
             render={(props) => (
-              <GetClubByAbbr {...props} />
+              <Club {...props} />
             )}
           />
         </Switch>
@@ -45,7 +57,7 @@ class Clubs extends React.Component {
 }
 
 Clubs.propTypes = {
-
+  match: PropTypes.object,
 };
 
 export default Clubs;
